ef9495b4b77058734fb22d368b4e3f2d657bc469
Michal Mucha <mjmucha@icloud.com>
2019-11-01 23:48:25 +0000

    getting home details

--- download_listing.py
+++ download_listing.py
@@ -1,8 +1,25 @@
-# download rightmove page -> price, name, location, photo
-
+# download browsed rightmove page -> price, name, location, photo
+import requests
+import bs4
+import re
 from typing import Tuple
+
 def get_listing(url:str) -> Tuple[int, str, str, str]:
-    return (250_000, 
-            'Pretty home', 
-            'Richmond, Surrey', 
-            'https://unsplash.com/home.png')
+    try:
+        response = requests.get(url)
+        soup = bs4.BeautifulSoup(response.content)
+        price = (soup.find('p', attrs={'id': 'propertyHeaderPrice'})
+                 .text.strip())
+        price = int(re.sub(r'([,£])', '', price))
+        return (
+            price,
+            soup.find('h1', attrs={'itemprop': 'name'}).text,
+            soup.find('meta', attrs={'itemprop': 'streetAddress'})['content'],
+            soup.find('meta', attrs={'name': 'twitter:image:src'})['content']
+        )
+    except:
+        print('Oops, are you sure about that URL?')
+        return (750_000, 
+                'Pretty home', 
+                'Richmond, Surrey', 
+                'https://unsplash.com/home.png')
